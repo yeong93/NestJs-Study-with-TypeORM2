ae7cceeae8d1f82238a98c71f35a396d112b3cb8
sukyeong <sasa02336@gmail.com>
2025-08-20 11:39:54 +0900

    add typeORM modules

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CatsController } from './cats.controller';
 import { CatsService } from './cats.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Cat } from './entity/cats.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Cat])],
+  exports: [TypeOrmModule],
   controllers: [CatsController],
   providers: [CatsService]
 })
