2c89092611dac99fbef9517207255ce8bfbc2faf
sukyeong <sasa02336@gmail.com>
2025-08-21 17:09:47 +0900

    add Nestjs Config, ConfigService

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
-import { Cat } from './domain/cats.entity';
 import { AuthModule } from './auth/auth.module';
-import { User } from "src/domain/user.entity";
-import { UserAuthority } from "src/domain/user-authority.entity";
 import { ormConfig } from './orm.config';
+import { ConfigModule} from '@nestjs/config';
+import config from './config/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      load: [config],
+      isGlobal: true
+    }),
     TypeOrmModule.forRootAsync({ useFactory: ormConfig}),
     CatsModule,
     AuthModule
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  // const port = process.env.NODE_SERVER_PORT;
+  const port = configService.get<string>('server.port');
   app.use(cookieParser());
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(port ?? 3000);
 }
 bootstrap();
